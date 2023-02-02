857620030772d85a3213d8da82889b3530cb8a73
“deshan68” <arundeshan@gmail.com>
2023-02-02 01:34:54 -0800

    clip board button complete

--- src/BoxScreen.jsx
+++ src/BoxScreen.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import BlurButton from "./BlurButton";
 import DrawerButton from "./DrawerButton";
 import SpreadButton from "./SpreadButton";
 import VoffsetButton from "./VoffsetButton";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 export default function BoxScreen() {
   const color = "gray";
@@ -10,6 +11,26 @@ export default function BoxScreen() {
   const [vOffSet, setVOffSet] = useState(0);
   const [blur, setBlur] = useState(0);
   const [spread, setSpread] = useState(0);
+  const [cssVal, setCssVal] = useState("");
+  const [copied, setCopied] = useState(false);
+
+  let copyText = document.getElementsByClassName("cssVal");
+  console.log(copyText.value);
+  // copyText.select();
+  // navigator.clipboard.writeText(copyText.value);
+  useEffect(() => {
+    setCssVal(
+      `boxShadow: ${mousePoint}px ${vOffSet}px ${blur}px ${spread}px ${color};`
+    );
+  });
+
+  // const copyToClipBoardHandler = () => {
+  //   console.log(cssVal);
+  // };
+
+  const copyToClipBoardHandler = () => {
+    setCopied(true);
+  };
 
   return (
     <>
@@ -32,25 +53,21 @@ export default function BoxScreen() {
       >
         Reset
       </button>
-      <button
-        type="button"
-        style={{
-          all: "unset",
-          backgroundColor: "green",
-          margin: "10px",
-          padding: "8px",
-          borderRadius: "5px",
-          cursor: "pointer",
-        }}
-        onClick={() => {
-          setBlur(0);
-          setMousePoint(0);
-          setVOffSet(0);
-          setSpread(0);
-        }}
-      >
-        Copy CSS values
-      </button>
+      <CopyToClipboard onCopy={copyToClipBoardHandler} text={cssVal}>
+        <button
+          type="button"
+          style={{
+            all: "unset",
+            backgroundColor: "green",
+            margin: "10px",
+            padding: "8px",
+            borderRadius: "5px",
+            cursor: "pointer",
+          }}
+        >
+          Copy CSS values
+        </button>
+      </CopyToClipboard>
       <div
         className="screen-log"
         style={{
@@ -67,8 +84,9 @@ export default function BoxScreen() {
         <SpreadButton spread={spread} setSpread={setSpread} />
       </div>
       <div className="cssVal">
-        boxShadow: {mousePoint}px {vOffSet}px {blur}px {spread}px {color}
+        boxShadow: {mousePoint}px {vOffSet}px {blur}px {spread}px {color};
       </div>
+      {copied ? <p style={{ color: "red" }}>Copied!</p> : null}
     </>
   );
 }
